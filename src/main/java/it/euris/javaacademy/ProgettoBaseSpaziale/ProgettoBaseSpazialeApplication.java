58ae2f4cf38b70c1761b9d6f6388d3b2e75c6512
magneee <biancadefilippi54@gmail.com>
2023-10-17 10:03:22 +0200

    First Commit

---
+++ src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/ProgettoBaseSpazialeApplication.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.ProgettoBaseSpaziale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBaseSpazialeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBaseSpazialeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/euris/javaacademy/ProgettoBaseSpaziale/ProgettoBaseSpazialeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.ProgettoBaseSpaziale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBaseSpazialeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
