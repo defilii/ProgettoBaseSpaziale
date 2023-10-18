47bd2685c7589b45d5e1a48f897c1f47782eb622
Xhuljano Hamzallari <xhuljano.hamzallari@gmail.com>
2023-10-18 15:50:12 +0200

    implemented CheckListController

--- src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/CheckListController.java
+++ src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/CheckListController.java
@@ -5,12 +5,15 @@ import it.euris.javaacademy.ProgettoBaseSpaziale.dto.ChecklistDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.dto.CheckmarkDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Checklist;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Checkmark;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustBeNullException;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustNotBeNullException;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.ChecklistService;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.CheckmarkService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 @AllArgsConstructor
@@ -25,5 +28,38 @@ public class CheckListController {
             """)
     public List<ChecklistDTO> getAllChecklists() {
         return checklistService.findAll().stream().map(Checklist::toDto).toList();
+    }
+    @PostMapping("/v1")
+    public ChecklistDTO saveChecklist(@RequestBody ChecklistDTO checklistDTO) {
+        try{
+            Checklist checklist = checklistDTO.toModel();
+            return checklistService.insert(checklist).toDto();
+        }
+        catch(IdMustBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PostMapping("/v1")
+    public ChecklistDTO updateChecklist(@RequestBody ChecklistDTO checklistDTO) {
+        try{
+            Checklist checklist = checklistDTO.toModel();
+            return checklistService.update(checklist).toDto();
+        }
+        catch(IdMustNotBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/v1/{id}")
+    public Boolean deleteChecklist(@PathVariable("id") Integer idChecklist){
+        return checklistService.deleteById(idChecklist);
+    }
+
+    @GetMapping("/v1/{id}")
+    public ChecklistDTO getChecklistById(@PathVariable("id") Integer idChecklist){
+        return checklistService.findById(idChecklist).toDto();
     }
 }
