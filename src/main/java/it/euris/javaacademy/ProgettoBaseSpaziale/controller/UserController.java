d8803dba51dd43d130e9ab6f6c5e9506f0d023a9
magneee <biancadefilippi54@gmail.com>
2023-10-18 15:49:11 +0200

    wrote get by id, put, post and delete methods in user controller

--- src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/UserController.java
+++ src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/UserController.java
@@ -1,12 +1,15 @@
 package it.euris.javaacademy.ProgettoBaseSpaziale.controller;
+
 import io.swagger.v3.oas.annotations.Operation;
 import it.euris.javaacademy.ProgettoBaseSpaziale.dto.UserDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.User;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustBeNullException;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustNotBeNullException;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,9 +22,53 @@ public class UserController {
 
     @GetMapping("/v1")
     @Operation(description = """
-      This method is used to retrieve all the users from the database<br>
-      """)
+            This method is used to retrieve all the users from the database<br>
+            """)
     public List<UserDTO> getAllUser() {
         return userService.findAll().stream().map(User::toDto).toList();
+    }
+
+    @GetMapping("/v1/{id}")
+    @Operation(description = """
+            This method is used to retrieve one user from the database<br>
+            """)
+    public UserDTO getUserById(@PathVariable("id") Integer idUser) {
+        return userService.findById(idUser).toDto();
+    }
+
+    @PostMapping("/v1")
+    @Operation(description = """
+            This method is used to save one user in the database<br>
+            """)
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        try {
+            User user = userDTO.toModel();
+            return userService.insert(user).toDto();
+        } catch (IdMustBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/v1")
+    @Operation(description = """
+            This method is used to update one user in the database<br>
+            """)
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        try {
+            User user = userDTO.toModel();
+            return userService.update(user).toDto();
+        } catch (IdMustNotBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/v1/{id}")
+    @Operation(description = """
+            This method is used to delete oen user from the database<br>
+            """)
+    public Boolean deleteUser(@PathVariable("id") Integer idUser) {
+        return userService.deleteById(idUser);
     }
 }
