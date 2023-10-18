f77ae79d2ffc65d28f931dd03d715f5747400db5
Xhuljano Hamzallari <xhuljano.hamzallari@gmail.com>
2023-10-18 16:12:03 +0200

    completed CheckListController, CheckmarkController, CommentoController e TabellaController

--- src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/ChecklistController.java
+++ src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/ChecklistController.java
@@ -2,24 +2,21 @@ package it.euris.javaacademy.ProgettoBaseSpaziale.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import it.euris.javaacademy.ProgettoBaseSpaziale.dto.ChecklistDTO;
-import it.euris.javaacademy.ProgettoBaseSpaziale.dto.CheckmarkDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Checklist;
-import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Checkmark;
 import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustBeNullException;
 import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustNotBeNullException;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.ChecklistService;
-import it.euris.javaacademy.ProgettoBaseSpaziale.service.CheckmarkService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/checklists")
-public class CheckListController {
+public class ChecklistController {
     ChecklistService checklistService;
 
     @GetMapping("/v1")
@@ -29,37 +26,36 @@ public class CheckListController {
     public List<ChecklistDTO> getAllChecklists() {
         return checklistService.findAll().stream().map(Checklist::toDto).toList();
     }
+
     @PostMapping("/v1")
     public ChecklistDTO saveChecklist(@RequestBody ChecklistDTO checklistDTO) {
-        try{
+        try {
             Checklist checklist = checklistDTO.toModel();
             return checklistService.insert(checklist).toDto();
-        }
-        catch(IdMustBeNullException e) {
+        } catch (IdMustBeNullException e) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
-    @PostMapping("/v1")
+    @PutMapping("/v1")
     public ChecklistDTO updateChecklist(@RequestBody ChecklistDTO checklistDTO) {
-        try{
+        try {
             Checklist checklist = checklistDTO.toModel();
             return checklistService.update(checklist).toDto();
-        }
-        catch(IdMustNotBeNullException e) {
+        } catch (IdMustNotBeNullException e) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
     @DeleteMapping("/v1/{id}")
-    public Boolean deleteChecklist(@PathVariable("id") Integer idChecklist){
+    public Boolean deleteChecklist(@PathVariable("id") Integer idChecklist) {
         return checklistService.deleteById(idChecklist);
     }
 
     @GetMapping("/v1/{id}")
-    public ChecklistDTO getChecklistById(@PathVariable("id") Integer idChecklist){
+    public ChecklistDTO getChecklistById(@PathVariable("id") Integer idChecklist) {
         return checklistService.findById(idChecklist).toDto();
     }
 }
--- src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/TabellaController.java
+++ src/main/java/it/euris/javaacademy/ProgettoBaseSpaziale/controller/TabellaController.java
@@ -5,12 +5,14 @@ import it.euris.javaacademy.ProgettoBaseSpaziale.dto.TabellaDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.dto.TaskDTO;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Tabella;
 import it.euris.javaacademy.ProgettoBaseSpaziale.entity.Task;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustBeNullException;
+import it.euris.javaacademy.ProgettoBaseSpaziale.exceptions.IdMustNotBeNullException;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.TabellaService;
 import it.euris.javaacademy.ProgettoBaseSpaziale.service.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,7 +26,39 @@ public class TabellaController {
     @Operation(description = """
             This method is used to retrieve all the tables from the database<br>
             """)
-    public List<TabellaDTO> getAllTabella() {
+    public List<TabellaDTO> getAllTabelle() {
         return tabellaService.findAll().stream().map(Tabella::toDto).toList();
+    }
+
+    @PostMapping("/v1")
+    public TabellaDTO saveTabella(@RequestBody TabellaDTO tabellaDTO) {
+        try {
+            Tabella tabella = tabellaDTO.toModel();
+            return tabellaService.insert(tabella).toDto();
+        } catch (IdMustBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PutMapping("/v1")
+    public TabellaDTO updateTabella(@RequestBody TabellaDTO tabellaDTO) {
+        try {
+            Tabella tabella = tabellaDTO.toModel();
+            return tabellaService.update(tabella).toDto();
+        } catch (IdMustNotBeNullException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/v1/{id}")
+    public Boolean deleteTabella(@PathVariable("id") Integer idTabella) {
+        return tabellaService.deleteById(idTabella);
+    }
+
+    @GetMapping("/v1/{id}")
+    public TabellaDTO getTabellaById(@PathVariable("id") Integer idTabella) {
+        return tabellaService.findById(idTabella).toDto();
     }
 }
